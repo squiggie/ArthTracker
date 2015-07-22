60dd0b82c31a6a03271d77bd4ba77a4cd7c67039
Landen <squigge@gmail.com>
2015-07-21 23:01:31 -0500

    add circleview

--- app/src/main/java/com/arthtracker/arthtracker/RVAdapter.java
+++ app/src/main/java/com/arthtracker/arthtracker/RVAdapter.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import com.github.pavlospt.CircleView;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
@@ -30,15 +30,15 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.PersonViewHolder> 
         TextView date;
         TextView stiffness;
         TextView overall;
-        ImageView circle;
+        CircleView circle;
 
         PersonViewHolder(View itemView) {
             super(itemView);
             cv = (CardView)itemView.findViewById(R.id.cv);
             //fatigue = (TextView)itemView.findViewById(R.id.fatigue);
             date = (TextView)itemView.findViewById(R.id.date);
-            overall = (TextView)itemView.findViewById(R.id.score);
-            circle = (ImageView)itemView.findViewById((R.id.circle));
+            //overall = (TextView)itemView.findViewById(R.id.score);
+            circle = (CircleView)itemView.findViewById((R.id.circle));
         }
     }
 
@@ -60,16 +60,22 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.PersonViewHolder> 
         //personViewHolder.fatigue.setText("Fatigue: " + painDays.get(i).getmFatigue());
         ;
         personViewHolder.date.setText(sdf.format(painDays.get(i).getmDate()));
-        personViewHolder.overall.setText(String.valueOf((painDays.get(i).getmOverall())));
+        //personViewHolder.overall.setText(String.valueOf((painDays.get(i).getmOverall())));
+        personViewHolder.circle.setTitleText(String.valueOf((painDays.get(i).getmOverall())));
         if (painDays.get(i).getmOverall() <= 33){
-            personViewHolder.circle.setBackground(context.getResources().getDrawable(R.drawable.red_circle));
+            //personViewHolder.circle.setBackground(context.getResources().getDrawable(R.drawable.red_circle));
+            personViewHolder.circle.setStrokeColor(context.getResources().getColor(R.color.red));
         }
         else if ((painDays.get(i).getmOverall() > 33) && (painDays.get(i).getmOverall() <= 66)){
-            personViewHolder.circle.setBackground(context.getResources().getDrawable(R.drawable.yellow_circle));
-            personViewHolder.overall.setTextColor(Color.BLACK);
+            //personViewHolder.circle.setBackground(context.getResources().getDrawable(R.drawable.yellow_circle));
+            //personViewHolder.overall.setTextColor(Color.BLACK);
+            personViewHolder.circle.setStrokeColor(context.getResources().getColor(R.color.yellow));
+
         }
         else{
-            personViewHolder.circle.setBackground(context.getResources().getDrawable(R.drawable.green_circle));
+            //personViewHolder.circle.setBackground(context.getResources().getDrawable(R.drawable.green_circle));
+            personViewHolder.circle.setStrokeColor(context.getResources().getColor(R.color.green));
+
         }
 
     }
