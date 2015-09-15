b3a49c05fa54b25c4fe09dd5a5b44151bea10d75
unknown <Landen>
2015-09-15 14:37:14 -0500

    add graph items

--- app/src/main/java/com/arthtracker/arthtracker/GraphActivity.java
+++ app/src/main/java/com/arthtracker/arthtracker/GraphActivity.java
@@ -1,6 +1,7 @@
 package com.arthtracker.arthtracker;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -8,8 +9,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class GraphActivity extends ActionBarActivity {
     private Toolbar toolbar;
+    private LineChart mChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +34,18 @@ public class GraphActivity extends ActionBarActivity {
 
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mChart = (LineChart) findViewById(R.id.chart);
+
+        SQLiteHelper sqlHelper = new SQLiteHelper(this);
+        List<PainDay> painDays = sqlHelper.getAllPainDays();
+
+        //Populate Chart
+        setData(painDays.size(), 100, painDays);
+
+        mChart.animateX(2500, Easing.EasingOption.EaseInOutQuart);
+        mChart.setDescription("Pain Over Time");
+        mChart.setScaleEnabled(true);
     }
 
     @Override
@@ -51,5 +76,48 @@ public class GraphActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setData(int count, float range, List<PainDay> painDays) {
+        SimpleDateFormat sdf = new SimpleDateFormat("MMM d, ''yy");
+
+        ArrayList<String> xVals = new ArrayList<String>();
+        for (int i = 0; i < count; i++) {
+            xVals.add(sdf.format(new Date(painDays.get(i).getmDate() * 1000)));
+        }
+
+        ArrayList<Entry> yVals = new ArrayList<Entry>();
+
+        for (int i = 0; i < count; i++) {
+            yVals.add(new Entry((float)painDays.get(i).getmTotal(), i));
+        }
+
+        // create a dataset and give it a type
+        LineDataSet set1 = new LineDataSet(yVals, "Date");
+        // set1.setFillAlpha(110);
+        // set1.setFillColor(Color.RED);
+
+        // set the line to be drawn like this "- - - - - -"
+        set1.enableDashedLine(10f, 5f, 0f);
+        set1.setColor(Color.BLACK);
+        set1.setCircleColor(Color.BLACK);
+        set1.setLineWidth(1f);
+        set1.setCircleSize(3f);
+        set1.setDrawCircleHole(false);
+        set1.setValueTextSize(9f);
+        set1.setFillAlpha(65);
+        set1.setFillColor(Color.BLACK);
+//        set1.setDrawFilled(true);
+        // set1.setShader(new LinearGradient(0, 0, 0, mChart.getHeight(),
+        // Color.BLACK, Color.WHITE, Shader.TileMode.MIRROR));
+
+        ArrayList<LineDataSet> dataSets = new ArrayList<LineDataSet>();
+        dataSets.add(set1); // add the datasets
+
+        // create a data object with the datasets
+        LineData data = new LineData(xVals, dataSets);
+
+        // set data
+        mChart.setData(data);
     }
 }
